439fe226768fc85150ffb91e8a0c681ef328c44f
Sam Harris <sam.harris@southeasttech.edu>
2024-10-18 22:07:30 -0500

    program edited two

--- showtracker/Program.cs
+++ showtracker/Program.cs
@@ -4,13 +4,27 @@ using ShowTracker.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
-
-
 builder.Services.AddDbContext<ShowTrackerContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ShowTrackerContext>();
+    context.Database.Migrate();
+
+    // Seed Data
+    if (!context.Shows.Any())
+    {
+        context.Shows.AddRange(
+            new Show { Title = "Breaking Bad", Genre = "Drama", Rating = 10 },
+            new Show { Title = "Stranger Things", Genre = "Sci-Fi", Rating = 9 }
+        );
+        context.SaveChanges();
+    }
+}
 
 if (!app.Environment.IsDevelopment())
 {
